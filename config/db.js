27aa85170321fd4c5f82e36f14968fc4343eba92
backbechershuun <backbechershuun@gmail.com>
2026-09-22 10:59:37 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ let isConnected = false;
 const connectDB = async () => {
   if (isConnected) return;
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI);
+    const db = await mongoose.connect(process.env.MONGODB_URI);
     isConnected = db.connections[0].readyState === 1;
     console.log("MongoDB connected");
   } catch (err) {
